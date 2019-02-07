293e88e3ba3bfbc3201ae52b3ebe06bc504b62ae
Joren Vrancken <jorenvrancken@gmail.com>
2019-02-07 19:35:30 +0100

    Add models for issue #8

--- website/registrations/models.py
+++ website/registrations/models.py
@@ -1,14 +1,67 @@
+from enum import Enum
+
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.models import User
+
+User = get_user_model()
+
+
+class RoleChoice(Enum):
+    se = "SE Student"
+    sdm = "SDM Student"
+    director = "Director"
+    admin = "Admin"
+
+
+class SeasonChoice(Enum):
+    fall = "Fall"
+    spring = "Spring"
 
 
 class GiPHouseProfile(models.Model):
-
     user = models.OneToOneField(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
 
     github_id = models.IntegerField(
-        unique=True
+        unique=True,
+    )
+
+    github_username = models.TextField(
+        unique=True,
+    )
+
+    snumber = models.IntegerField(
+        unique=True,
+        null=True,
+    )
+
+    role = models.CharField(
+        max_length=8,
+        choices=[(tag, tag.value) for tag in RoleChoice]
+    )
+
+
+class Semester(models.Model):
+    year = models.IntegerField()
+    semester = models.CharField(
+        max_length=5,
+        choices=[(tag, tag.value) for tag in SeasonChoice]
+    )
+
+
+class Project(models.Model):
+    name = models.TextField()
+
+    academic_year = models.ForeignKey(
+        Semester,
+        on_delete=models.DO_NOTHING
+    )
+
+    description = models.TextField()
+
+    members = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING
     )
