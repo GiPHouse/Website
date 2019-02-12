0071b7a481deb2cdb29d979141738be305640fbd
Joren Vrancken <jorenvrancken@gmail.com>
2019-02-12 16:43:00 +0100

    Add tests and documentation to github_oauth (#26)
    * Remove testing code from GithubOAuthBackend
    
    * Split too long lines
    
    * Add docs to github_oauth
    
    * Add tests to github_oauth

--- website/github_oauth/views.py
+++ website/github_oauth/views.py
@@ -9,20 +9,26 @@ from django.contrib import messages
 
 @require_http_methods(['GET', ])
 def github_callback(request):
+    """
+    Callback view accessed by GitHub after an authorization request.
+    :param request: Object containing information about request user made.
+    :return: Redirect to homepage with a login status message.
+    """
 
     if 'code' not in request.GET or request.user.is_authenticated:
         return HttpResponseNotFound()
 
     code = request.GET['code']
 
-    user = authenticate(
-        request,
-        code=code
-    )
+    user = authenticate(request, code=code)
 
     if user is not None:
         login(request, user)
-        messages.success(request, 'Login Successful', extra_tags='alert alert-success')
+
+        messages.success(
+            request, 'Login Successful', extra_tags='alert alert-success'
+        )
+
         return redirect('home')
 
     messages.warning(request, 'Login Failed', extra_tags='alert alert-danger')
