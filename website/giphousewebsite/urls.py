11130a8bfd047c23a16e6b6d92af7e27e352ab17
BierBrigadier <39705148+BierBrigadier@users.noreply.github.com>
2019-02-13 16:40:29 +0100

    WIP: Static pages from giphouse.nl (#19)
    * add wayofworking page
    
    * about and project page were created.
    
    * Added links in the header
    
    * add buttons in header
    
    * Added page titles.

--- website/giphousewebsite/urls.py
+++ website/giphousewebsite/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('oauth/', include('github_oauth.urls')),
+    path('about/wayofworking', TemplateView.as_view(template_name='wayofworking.html'), name='wayofworking'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('clients/', TemplateView.as_view(template_name='clients.html'), name='clients'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ]
