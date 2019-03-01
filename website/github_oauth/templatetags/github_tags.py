d24c61044cb9ccc79e19c9c18dcc09771dcef841
Joren Vrancken <jorenvrancken@gmail.com>
2019-03-01 10:00:14 +0100

    Update github_oauth docs using PEP 257 (#76)

--- website/github_oauth/templatetags/github_tags.py
+++ website/github_oauth/templatetags/github_tags.py
@@ -9,14 +9,14 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def url_github_callback(context, callback_action=''):
     """
-    Tag used to load GitHub login/authorization url into templates
+    Tag used to load GitHub login/authorization url into templates.
+
     :param context: context of the request that requested the template.
     :param callback_action: An optional url name from github_oauth
            to redirect to after successful authentication.
     :return: url to request GitHub OAuth authentication
              with an optional redirect.
     """
-
     if callback_action:
         request = context['request']
         callback = request.build_absolute_uri(
