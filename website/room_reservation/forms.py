492411a5fcf4959ee9f33a2a00076104c7100115
Wietze Mulder <wietzem@gmail.com>
2019-03-08 15:50:53 +0100

    using tempus-dominus for datetime selection

--- website/room_reservation/forms.py
+++ website/room_reservation/forms.py
@@ -2,7 +2,7 @@ from django.contrib.admin import widgets
 from django.forms import ModelForm
 from django import forms
 from .models import Reservation
-
+from tempus_dominus.widgets import DateTimePicker
 
 class ReservationForm(ModelForm):
     class Meta:
@@ -11,15 +11,15 @@ class ReservationForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.fields['start_time'].widget = widgets.AdminSplitDateTime()
+        self.fields['start_time'].widget = DateTimePicker()
         #self.fields['end_time'].widget = forms.PasswordInput()
-        self.fields['start_time'].input_formats = ['%d/%m/%Y %H:%M']
-        self.fields['start_time'].widget = forms.DateTimeInput(attrs={
-            'class': 'form-control datetimepicker-input',
-            'data-target': '#datetimepicker1'
-        })
-        self.fields['end_time'].input_formats = ['%d/%m/%Y %H:%M']
-        self.fields['end_time'].widget = forms.DateTimeInput(attrs={
-            'class': 'form-control datetimepicker-input',
-            'data-target': '#datetimepicker2'
-        })
+        #self.fields['start_time'].input_formats = ['%d/%m/%Y %H:%M']
+        #self.fields['start_time'].widget = forms.DateTimeInput(attrs={
+        #    'class': 'form-control datetimepicker-input',
+        #    'data-target': '#datetimepicker1'
+        #})
+        #self.fields['end_time'].input_formats = ['%d/%m/%Y %H:%M']
+        #self.fields['end_time'].widget = forms.DateTimeInput(attrs={
+        #    'class': 'form-control datetimepicker-input',
+        #    'data-target': '#datetimepicker2'
+        #})
