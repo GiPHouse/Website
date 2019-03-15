21dc227d317742453562958c37fbc13826c5cc38
Jelle Besseling <jelle@pingiun.com>
2019-03-15 14:53:03 +0100

    Resolve "Create an option to make the Peer review temporarily available (#55)" (#130)
    * Change Questionnaire to use temporal fields
    
    * Add Questionnaire migration
    
    * Add late answer registration

--- website/peer_review/models.py
+++ website/peer_review/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from enum import Enum
 
+from django.utils import timezone
+
 
 class ScaleLabels(Enum):
     """Possible answer scales."""
@@ -25,11 +27,23 @@ class QuestionTypes(Enum):
     open_question = 'Open Question'
 
 
+class QuestionnaireManager(models.Manager):
+    """Manager for the Questionnaire model."""
+
+    def open_questionnaires(self):
+        """Return the questionnaires that are available at the current time."""
+        return self.filter(available_from__lte=timezone.now(),
+                           available_until__gte=timezone.now())
+
+
 class Questionnaire(models.Model):
     """A group of questions."""
 
     title = models.CharField(max_length=200)
-    active = models.BooleanField()
+    available_from = models.DateTimeField()
+    available_until = models.DateTimeField()
+
+    objects = QuestionnaireManager()
 
     def __str__(self):
         """Return title."""
@@ -84,6 +98,18 @@ class Answer(models.Model):
         blank=True,
         null=True
     )
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    on_time = models.BooleanField()
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        """Save model and set on_time field."""
+        if self.created:
+            self.on_time = self.created < self.question.questionnaire.available_until
+        else:
+            self.on_time = timezone.now() < self.question.questionnaire.available_until
+        super().save(force_insert, force_update, using, update_fields)
 
     def __str__(self):
         """Return information about answer as string."""
