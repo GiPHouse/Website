5fbf4105f35aec77bf892cd6a50390646935b4f6
Joren Vrancken <jorenvrancken@gmail.com>
2019-04-08 09:05:39 +0200

    Use class based views for github_auth app

--- website/github_oauth/urls.py
+++ website/github_oauth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from github_oauth.views import github_login, github_register
+from github_oauth.views import GithubLoginView, GithubRegisterView
 
 app_name = 'github_oauth'
 urlpatterns = [
-    path('login/', github_login, name='login'),
-    path('register/', github_register, name='register')
+    path('login/', GithubLoginView.as_view(), name='login'),
+    path('register/', GithubRegisterView.as_view(), name='register')
 ]
