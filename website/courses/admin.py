c037e0ea4b7c9cf5fbcd0ffd18dcbd575839a995
arne <a.diehl@student.ru.nl>
2019-04-10 14:29:27 +0200

    improve docstrings

--- website/courses/admin.py
+++ website/courses/admin.py
@@ -7,7 +7,7 @@ from courses.models import Semester, Lecture, Course
 
 
 def year_choices():
-    "Return years from 2008 to next year."
+    """Return years from 2008 to next year."""
     return [(r, r) for r in range(2008, timezone.now().year+2)]
 
 
@@ -18,19 +18,21 @@ class LectureAdmin(admin.ModelAdmin):
     list_filter = ('course', 'semester__season', 'semester__year', 'teacher')
 
 
-class SemesterForm(forms.ModelForm):
+class AdminSemesterForm(forms.ModelForm):
     """Semester Object form with typed choices for the year."""
 
     year = forms.TypedChoiceField(
         coerce=int, choices=year_choices, initial=timezone.now().year)
 
     class Meta:
-        "Link SemesterForm to Semester model."
+        """Link SemesterForm to Semester model."""
+
         model = Semester
         exclude = []
 
 
-class SemesterAdmin(admin.ModelAdmin):
+@admin.register(Semester)
+class AdminSemesterForm(admin.ModelAdmin):
     """Admin for the Semester Object using a custom form."""
 
     form = SemesterForm
