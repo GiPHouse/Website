da05ba2d17a7037925a8bf5455fe10e8e064dda3
Joren Vrancken <jorenvrancken@gmail.com>
2019-04-10 15:52:53 +0200

    Use class based views for github_auth app (#143)

--- website/github_oauth/views.py
+++ website/github_oauth/views.py
@@ -1,82 +1,87 @@
+from django.views import View
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, get_user_model
-from django.views.decorators.http import require_http_methods
-
+from django.conf import settings
 from django.http.response import HttpResponseBadRequest
 from django.contrib import messages
+
 from github_oauth.backends import GithubOAuthBackend
 
 User = get_user_model()
 
 
-@require_http_methods(['GET', ])
-def github_login(request):
-    """
-    View accessed by GitHub after an authorization request of a user trying to login.
+class BaseGithubView(View):
+    """Base View for Github OAuth."""
 
-    :param request: Object containing information about request user made.
-    :return: Redirect to homepage with a login status message.
-    """
-    if 'code' not in request.GET:
-        return HttpResponseBadRequest()
+    redirect_url_success = settings.LOGIN_REDIRECT_URL
+    redirect_url_failure = settings.LOGIN_REDIRECT_URL
 
-    if request.user.is_authenticated:
-        messages.warning(request, "You are already logged in", extra_tags='success')
-        return redirect('home')
+    def get(self, request, *args, **kwargs):
+        """Handle GET request made by Github OAuth."""
+        return redirect(self.redirect_url_success)
 
-    code = request.GET['code']
+    def dispatch(self, request, *args, **kwargs):
+        """Check if user is authenticated and if the code GET parameter exists."""
+        if request.method != 'GET':
+            return self.http_method_not_allowed(request, *args, **kwargs)
 
-    user = authenticate(request, code=code)
+        try:
+            self.code = request.GET['code']
+        except KeyError:
+            return HttpResponseBadRequest()
 
-    if user is not None:
-        login(request, user)
+        if request.user.is_authenticated:
+            messages.warning(request, "You are already logged in", extra_tags='success')
+            return redirect('home')
 
-        messages.success(
-            request, 'Login Successful', extra_tags='success'
-        )
-
-        return redirect('home')
-
-    messages.warning(request, 'Login Failed', extra_tags='danger')
-    return redirect('home')
+        return self.get(request, *args, **kwargs)
 
 
-@require_http_methods(['GET', ])
-def github_register(request):
-    """
-    View accessed by GitHub after an authorization request of a user trying to register.
+class GithubLoginView(BaseGithubView):
+    """View accessed by GitHub after an authorization request of a user trying to login."""
 
-    :param request: Object containing information about request user made.
-    :return: Redirect to homepage with a login status message.
-    """
-    if 'code' not in request.GET:
-        return HttpResponseBadRequest()
+    def get(self, request, *args, **kwargs):
+        """Handle GET request made by Github OAuth."""
+        user = authenticate(request, code=self.code)
 
-    if request.user.is_authenticated:
-        messages.warning(request, "You are already logged in", extra_tags='success')
-        return redirect('home')
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login Successful', extra_tags='success')
+            return redirect(self.redirect_url_failure)
 
-    code = request.GET['code']
+        messages.warning(request, 'Login Failed', extra_tags='danger')
+        return redirect(self.redirect_url_success)
 
-    backend = GithubOAuthBackend()
-    github_info = backend.get_github_info(code)
 
-    try:
-        user = User.objects.get(giphouseprofile__github_id=github_info['id'])
-    except User.DoesNotExist:
-        pass
-    else:
-        login(
-            request,
-            user,
-            backend='github_oauth.backends.GithubOAuthBackend',
-        )
-        return redirect('home')
+class GithubRegisterView(BaseGithubView):
+    """View accessed by GitHub after an authorization request of a user trying to register."""
 
-    session = request.session
-    session['github_id'] = github_info['id']
-    session['github_username'] = github_info['login']
-    session['github_email'] = github_info['email']
-    session['github_name'] = github_info['name']
+    redirect_url_success = reverse_lazy('registrations:step2')
 
-    return redirect('registrations:step2')
+    def get(self, request, *args, **kwargs):
+        """Handle GET request made by Github OAuth."""
+        backend = GithubOAuthBackend()
+        github_info = backend.get_github_info(self.code)
+
+        try:
+            user = User.objects.get(giphouseprofile__github_id=github_info['id'])
+        except User.DoesNotExist:
+            pass
+        else:
+            login(
+                request,
+                user,
+                backend='github_oauth.backends.GithubOAuthBackend',
+            )
+            messages.success(request, 'You already have an account.', extra_tags='success')
+            return redirect(self.redirect_url_failure)
+
+        request.session.update({
+                'github_id': github_info['id'],
+                'github_username': github_info['login'],
+                'github_email': github_info['email'],
+                'github_name': github_info['name'],
+        })
+
+        return redirect(self.redirect_url_success)
