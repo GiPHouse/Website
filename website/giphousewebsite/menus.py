f70291bac145a9e412e87e70e8df204fa787a181
Jelle Besseling <jelle@pingiun.com>
2019-04-17 09:01:48 +0200

    Make the styling of the website better

---
+++ website/giphousewebsite/menus.py
@@ -0,0 +1,57 @@
+"""
+This file defines the menu layout.
+
+We set the variable `:py:main` to form the menu tree.
+"""
+from django.urls import reverse
+
+from courses.models import Semester
+from peer_review.models import Questionnaire
+
+__all__ = ['MAIN_MENU']
+
+"""
+Defines the menu layout as a nested dict
+
+The authenticated key indicates something should only
+be visible for logged-in users. *Do not* rely on that for authentication!
+"""
+MAIN_MENU = [
+    {'title': 'Home', 'url': reverse('home')},
+    {
+        'title': 'About',
+        'submenu': [
+            {'title': 'About GiPHouse', 'url': reverse('about')},
+            {'title': 'Way of working', 'url': reverse('wayofworking')},
+        ],
+    },
+    {
+        'title': 'Course Content',
+        'submenu': lambda: [{'title': str(semester), 'url': reverse('courses:lectures',
+                                                                    args=(semester.year, semester.season))}
+                            for semester in Semester.objects.all()],
+    },
+    {
+        'title': 'Projects',
+        'submenu': lambda: [{'title': str(semester), 'url': reverse('projects:projects',
+                                                                    args=(semester.year, semester.season))}
+                            for semester in Semester.objects.all()],
+    },
+    {
+        'title': 'Contact',
+        'url': reverse('contact'),
+    },
+    {
+        'title': 'Peer Review',
+        'visible': lambda request: (Questionnaire.objects.open_questionnaires().count() > 1
+                                    and request.user.is_authenticated),
+        'url': reverse('peer_review:overview')
+    },
+    {
+        'title': 'Peer Review',
+        'visible': lambda request: (Questionnaire.objects.open_questionnaires().count() == 1 and
+                                    request.user.is_authenticated),
+        'url': lambda: (reverse('peer_review:answer', args=(Questionnaire.objects.first().pk,))
+                        if Questionnaire.objects.first() else False)
+    }
+]
