b1c58f4e36d4671d3048705d278a855ef92120d8
Joren Vrancken <jorenvrancken@gmail.com>
2019-05-17 15:39:55 +0200

    Shorten and reorder reservation form clean method. (#255)

--- website/room_reservation/forms.py
+++ website/room_reservation/forms.py
@@ -30,43 +30,32 @@ class ReservationForm(ModelForm):
         - Reservation is not too long.
         """
         cleaned_data = super().clean()
-        room = cleaned_data.get("room")
         start_time = cleaned_data.get("start_time")
         end_time = cleaned_data.get("end_time")
 
-        pk = cleaned_data.get("pk")
-
-        already_taken = Reservation.objects.filter(
-            room=room,
-        ).filter(
-            Q(
-                start_time__lte=start_time,
-                end_time__gt=start_time,)
-            | Q(
-                start_time__lt=end_time,
-                end_time__gte=end_time,
-            )
-            | Q(
-                start_time__gte=start_time,
-                end_time__lte=end_time,
-            )
-        ).exclude(pk=pk).exists()
-
-        if already_taken:
-            raise ValidationError(
-                'Room already reserved in this timeslot.', code='invalid')
-
         if end_time.date() - start_time.date() >= timedelta(days=1):
-            raise ValidationError(
-                'Rerservation too long. Please shorten your reservation', code='invalid')
+            raise ValidationError('Reservation too long. Please shorten your reservation', code='invalid')
 
         if start_time >= end_time:
-            raise ValidationError(
-                'start time past or same as end time. Please enter a valid time range.', code='invalid')
+            raise ValidationError('Start time needs to be before end time.', code='invalid')
 
         if start_time.hour < 8 or start_time.hour >= 18 or end_time.hour < 8 or start_time.hour > 18:
-            raise ValidationError(
-                    'reservation outside range, Please enter times between 8:00 and 18:00.', code='invalid')
+            raise ValidationError('Please enter times between 8:00 and 18:00.', code='invalid')
+
+        already_taken = (
+            Reservation.objects
+            .filter(room=cleaned_data.get("room"))
+            .filter(
+                Q(start_time__lte=start_time, end_time__gt=start_time)
+                | Q(start_time__lt=end_time, end_time__gte=end_time)
+                | Q(start_time__gte=start_time, end_time__lte=end_time)
+            )
+            .exclude(pk=cleaned_data.get("pk"))
+            .exists()
+        )
+
+        if already_taken:
+            raise ValidationError('Room already reserved in this timeslot.', code='invalid')
 
     def __init__(self, *args, **kwargs):
         """Initialize the object and give user-friendly widgets for the datetime objects."""
