d5550070f5f68c566af1b92f9f2d2a758e70d975
Joren Vrancken <jorenvrancken@gmail.com>
2019-05-29 11:43:45 +0200

    Remove reservee filter

--- website/room_reservation/admin.py
+++ website/room_reservation/admin.py
@@ -8,7 +8,7 @@ class ReservationAdmin(admin.ModelAdmin):
     """Admin class for Reservation."""
 
     list_display = ('reservee', 'room', 'start_time', 'end_time')
-    list_filter = ('reservee', 'room', 'start_time', 'end_time')
+    list_filter = ('room', 'start_time', 'end_time')
 
 
 @admin.register(Room)
