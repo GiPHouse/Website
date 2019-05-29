829f5c354c9f4b3f3d4f4ead0e8733cf6903a25a
Joren Vrancken <jorenvrancken@gmail.com>
2019-05-29 11:04:18 +0200

    Disable adding and changing room reservations in the admin

--- website/room_reservation/admin.py
+++ website/room_reservation/admin.py
@@ -10,6 +10,14 @@ class ReservationAdmin(admin.ModelAdmin):
     list_display = ('reservee', 'room', 'start_time', 'end_time')
     list_filter = ('reservee', 'room', 'start_time', 'end_time')
 
+    def has_add_permission(self, request):
+        """Reservation should only be added through the frontend."""
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        """Reservation should only be changed through the frontend."""
+        return False
+
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
