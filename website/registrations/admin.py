f29b8be3d03790197d48a75bd929421de4256022
adiehl96 <a.diehl@student.ru.nl>
2019-05-30 19:14:30 +0200

    Update admin.py

--- website/registrations/admin.py
+++ website/registrations/admin.py
@@ -120,21 +120,21 @@ class StudentAdmin(admin.ModelAdmin):
         return self.model.objects.filter(giphouseprofile__isnull=False)
 
     def get_preference1(self, obj):
-        """Return github_username of Student."""
+        """Return 1st project preference of Student."""
         registration = obj.registration_set.order_by('semester').first()
         return registration.preference1 if registration else None
     get_preference1.short_description = 'Preference1'
     get_preference1.admin_order_field = 'giphouseprofile__github_username'
 
     def get_preference2(self, obj):
-        """Return github_username of Student."""
+        """Return 2nd project preference of Student."""
         registration = obj.registration_set.order_by('semester').first()
         return registration.preference2 if registration else None
     get_preference2.short_description = 'Preference2'
     get_preference2.admin_order_field = 'giphouseprofile__github_username'
 
     def get_preference3(self, obj):
-        """Return github_username of Student."""
+        """Return 3rd project preference of Student."""
         registration = obj.registration_set.order_by('semester').first()
         return registration.preference3 if registration else None
     get_preference3.short_description = 'Preference3'
