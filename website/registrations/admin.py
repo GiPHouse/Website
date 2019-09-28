c46460b8b2e32b8b23b49ee710fa038b92532a01
Joren Vrancken <jorenvrancken@gmail.com>
2019-09-28 12:51:45 +0200

    Add extra fields to User list_display

--- website/registrations/admin.py
+++ website/registrations/admin.py
@@ -17,43 +17,64 @@ class RegistrationInline(admin.StackedInline):
 class UserAdmin(admin.ModelAdmin):
     """Custom admin for Student."""
 
-    fields = ("first_name", "last_name", "email", "student_number", "github_id", "github_username", "date_joined")
-    inlines = [RegistrationInline]
-    list_display = ("full_name", "get_preference1", "get_preference2", "get_preference3")
     actions = ["place_in_first_project_preference"]
 
-    list_filter = ("registration__semester", "registration__project", "registration__course")
+    fields = ("first_name", "last_name", "email", "student_number", "github_id", "github_username", "date_joined")
+    inlines = [RegistrationInline]
+    list_display = (
+        "__str__",
+        "github_username",
+        "get_current_project",
+        "get_experience",
+        "get_preference1",
+        "get_preference2",
+        "get_preference3",
+    )
+
+    list_filter = (
+        "registration__semester",
+        "registration__project",
+        "registration__course",
+        "registration__experience",
+    )
 
     # Necessary for the autocomplete filter
-    search_fields = ("first_name", "last_name")
-
-    def full_name(self, obj):
-        """Return full name of student."""
-        return f"{obj.first_name} {obj.last_name}"
+    search_fields = ("first_name", "last_name", "student_number")
 
     def get_preference1(self, obj):
-        """Return 1st project preference of Student."""
+        """Return 1st project preference."""
         registration = obj.registration_set.first()
         return registration.preference1 if registration else None
 
-    get_preference1.short_description = "Preference1"
-    get_preference1.admin_order_field = "github_username"
+    get_preference1.short_description = "First Preference"
 
     def get_preference2(self, obj):
-        """Return 2nd project preference of Student."""
+        """Return 2nd project preference."""
         registration = obj.registration_set.first()
         return registration.preference2 if registration else None
 
     get_preference2.short_description = "Preference2"
-    get_preference2.admin_order_field = "github_username"
 
     def get_preference3(self, obj):
-        """Return 3rd project preference of Student."""
+        """Return 3rd project preference."""
         registration = obj.registration_set.first()
         return registration.preference3 if registration else None
 
     get_preference3.short_description = "Preference3"
-    get_preference3.admin_order_field = "github_username"
+
+    def get_experience(self, obj):
+        """Return experience."""
+        registration = obj.registration_set.first()
+        return registration.get_experience_display() if registration else None
+
+    get_experience.short_description = "Experience"
+
+    def get_current_project(self, obj):
+        """Return current project."""
+        registration = obj.registration_set.first()
+        return registration.project if registration else None
+
+    get_current_project.short_description = "Project"
 
     def place_in_first_project_preference(self, request, queryset):
         """Place the selected users in their first project preference."""
