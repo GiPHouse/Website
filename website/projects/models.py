a67871860718dd704d658c8f74e1382ea8720a54
Jelmer Hinssen <j.hinssen@student.ru.nl>
2020-02-26 17:04:02 +0100

    Changed imports to follow codestyle requirements

--- website/projects/models.py
+++ website/projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 
-from courses.models import Semester, Course
+from courses.models import Course, Semester
 
 from registrations.models import Employee
 
