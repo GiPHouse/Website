f79b19a548447f02b1027583016718c88bc4782b
Joren Vrancken <jorenvrancken@gmail.com>
2020-10-09 20:11:06 +0200

    Redirect www.giphouse.nl to giphouse.nl (#502)

--- website/giphousewebsite/settings/production.py
+++ website/giphousewebsite/settings/production.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['VIRTUAL_HOST']]
+ALLOWED_HOSTS = os.environ['VIRTUAL_HOST'].split(',')
 
 SESSION_COOKIE_SECURE = True
 
