0eeabbc503d86c1d7388db9f55c9b6292ec4d7e7
Joren Vrancken <jorenvrancken@gmail.com>
2021-04-12 10:59:23 +0200

    Update dependencies (#527)

--- website/giphousewebsite/settings/base.py
+++ website/giphousewebsite/settings/base.py
@@ -70,15 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'giphousewebsite.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
