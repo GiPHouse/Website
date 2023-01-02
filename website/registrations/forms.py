6d73aee4a63030b84a746b1c2f57107e6de84f73
Job Doesburg <job.doesburg@gmail.com>
2023-01-02 21:35:56 +0100

    Fix styling

--- website/registrations/forms.py
+++ website/registrations/forms.py
@@ -54,7 +54,13 @@ class Step2Form(forms.Form):
         label="What is your programming experience?",
         choices=Registration.EXPERIENCE_CHOICES,
         initial=Registration.EXPERIENCE_BEGINNER,
-        help_text="<strong>Beginner</strong>: I barely passed the programming courses from my curriculum.<br><strong>Intermediate</strong>: the programming courses in the curriculum were very easy for me and I have experience with some small (hobby) projects.<br><strong>Advanced</strong>: I have a lot of experience with programming.",
+        help_text="<strong>Beginner</strong>: I barely passed the programming "
+        "courses from my curriculum.<br>"
+        "<strong>Intermediate</strong>: the programming courses in "
+        "the curriculum were very easy for me and I have experience "
+        "with some small (hobby) projects.<br>"
+        "<strong>Advanced</strong>: I have a lot of experience with "
+        "programming.",
     )
 
     project1 = forms.ModelChoiceField(label="First project preference", queryset=None, required=False)
