48e2da5fe8f400e464a5d098680ad05af2d22192
Job Doesburg <job.doesburg@gmail.com>
2023-01-03 16:01:45 +0100

    Auto-forward users on login with wrong url (#605)
    * Auto-forward users on login with wrong url
    
    * Fix documentation
    
    * No coverage
    
    * Add pydocs

--- website/giphousewebsite/urls.py
+++ website/giphousewebsite/urls.py
@@ -2,11 +2,30 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
-from django.urls import include, path
-from django.views.generic import TemplateView
+from django.urls import include, path, reverse_lazy
+from django.views.generic import RedirectView, TemplateView
+
+from github_oauth.templatetags.github_tags import url_github_callback
+
+
+class GitHubLoginRedirectView(RedirectView):
+    """Redirect to GitHub login page."""
+
+    permanent = True
+    query_string = True
+    pattern_name = "login-redirect"
+
+    def get_redirect_url(self, *args, **kwargs):
+        """Return the redirect url for GitHub login."""
+        return url_github_callback(
+            {"request": self.request}, "login", next_url=reverse_lazy("admin:index")
+        )  # pragma: no cover
+
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("admin/login/", GitHubLoginRedirectView.as_view(), name="login-redirect"),
+    path("admin/logout/", RedirectView.as_view(url="/logout", query_string=True), name="logout-redirect"),
     path("admin/", admin.site.urls),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("oauth/", include("github_oauth.urls")),
