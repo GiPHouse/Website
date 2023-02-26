471f67be3f99a3d7d9e3bb075d7126ec7acfdebc
1058274 <70607431+1058274@users.noreply.github.com>
2023-02-26 12:54:36 +0100

    Add unit tests for creating AWS organization

--- website/projects/tests/test_awssync.py
+++ website/projects/tests/test_awssync.py
@@ -1,4 +1,12 @@
+from unittest.mock import patch
+
+import boto3
+
+from botocore.exceptions import ClientError
+
 from django.test import TestCase
+
+from moto import mock_organizations
 
 from projects import awssync
 
@@ -12,3 +20,43 @@ class AWSSyncTest(TestCase):
     def test_button_pressed(self):
         return_value = self.sync.button_pressed()
         self.assertTrue(return_value)
+
+    def mock_api(self, operation_name, kwarg):
+        if operation_name == "CreateOrganization":
+            raise ClientError(
+                {
+                    "Error": {
+                        "Message": "The AWS account is already a member of an organization.",
+                        "Code": "AlreadyInOrganizationException",
+                    },
+                    "ResponseMetadata": {
+                        "RequestId": "ffffffff-ffff-ffff-ffff-ffffffffffff",
+                        "HTTPStatusCode": 400,
+                        "HTTPHeaders": {
+                            "x-amzn-requestid": "ffffffff-ffff-ffff-ffff-ffffffffffff",
+                            "content-type": "application/x-amz-json-1.1",
+                            "content-length": "111",
+                            "date": "Sun, 01 Jan 2023 00:00:00 GMT",
+                            "connection": "close",
+                        },
+                        "RetryAttempts": 0,
+                    },
+                    "Message": "The AWS account is already a member of an organization.",
+                },
+                "create_organization",
+            )
+
+    @mock_organizations
+    def test_create_aws_organization(self):
+        moto_client = boto3.client("organizations")
+        org = self.sync
+        org.create_aws_organization()
+        describe_org = moto_client.describe_organization()["Organization"]
+        self.assertEqual(describe_org, org.org_info)
+
+    @patch("botocore.client.BaseClient._make_api_call", mock_api)
+    def test_create_aws_organization__exception(self):
+        org = self.sync
+        org.create_aws_organization()
+        self.assertTrue(org.fail)
+        self.assertIsNone(org.org_info)
