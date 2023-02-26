7786fcb75974a84db8159910920eb423accb4871
1058274 <70607431+1058274@users.noreply.github.com>
2023-02-26 12:32:14 +0100

    Add function to create AWS organization

--- website/projects/awssync.py
+++ website/projects/awssync.py
@@ -1,11 +1,18 @@
 import logging
 
+import boto3
+
+from botocore.exceptions import ClientError
+
+
 class AWSSync:
     """Synchronise with Amazon Web Services."""
 
     def __init__(self):
         """Create an AWSSync instance."""
         self.logger = logging.getLogger("django.aws")
+        self.org_info = None
+        self.fail = False
         self.logger.info("Created AWSSync instance.")
 
     def button_pressed(self):
@@ -16,3 +23,16 @@ class AWSSync:
         """
         self.logger.info("Pressed button")
         return True
+
+    def create_aws_organization(self):
+        """Create an AWS organization with the current user as the management account."""
+        client = boto3.client("organizations")
+        try:
+            response = client.create_organization(FeatureSet="ALL")
+            self.org_info = response["Organization"]
+            self.logger.info("Created an AWS organization and saved organization info.")
+        except ClientError as error:
+            self.fail = True
+            self.logger.error("Something went wrong creating an AWS organization.")
+            self.logger.debug(f"{error}")
+            self.logger.debug(f"{error.response}")
