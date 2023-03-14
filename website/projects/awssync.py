baf6f285374b161cd7f8d5b4c9a663192ecd7332
Jer111 <82157107+Jer111@users.noreply.github.com>
2023-03-14 10:35:27 +0100

    Db sync (#16)
    * Added button to /admin/projects/project page on website
    
    * Framework for AWS synchronisation button functionality
    
    * Linked synchronisation button to framework
    
    * Fixed import order to comply with flake8
    
    * Added docstring to synchronise_to_AWS
    
    * Changed redirect in synchronise_to_AWS function so that it redirects to the page from which the AWS sync button was pressed.
    
    * Added tests and changed awssync.py to allow for testing the button_pressed function.
    
    * Added get mailinglist to the awssync file
    
    * Added first version of get_all_managers
    
    * Added test case for mailing lists
    
    * Removed some prints
    
    * reformatted using black
    
    * flake8 intentation added
    
    * flake8 intentation correction
    
    * Removed get manager
    
    * Linting added
    
    * unused import removed
    
    * Added get_teamid_from_email
    
    * Changed function email with teamid
    
    * Updated get_emails_with_teamids, working now
    
    * Added test for get_emails_with_ids
    
    * Added linting
    
    * linting
    
    * Added more test
    
    * Linting in awssync and its test file
    
    * Moved the imports around
    
    * moved the imports around
    
    * Black linting
    
    * switched imports around
    
    * Switched imports around part 2
    
    * Switched imports around part 3
    
    * Switched imports around part 4
    
    * Fixed when no project exist for mailing list
    
    * Added some more tests
    
    * Removed exeption try expect
    
    * Black linting
    
    * Changed get_email_with_teamid to new format
    
    * Changed get_emails_with_teamids to go over project
    
    * Added tests for get_emails_with_teamids
    
    * changed info for get_emails_with_teamids
    
    ---------
    
    Co-authored-by: Henk <hb130502@outlook.com>

--- website/projects/awssync.py
+++ website/projects/awssync.py
@@ -1,8 +1,16 @@
+"""Framework for synchronisation with Amazon Web Services (AWS)."""
+
 import logging
 
 import boto3
 
 from botocore.exceptions import ClientError
+
+from courses.models import Semester
+
+from mailing_lists.models import MailingList
+
+from projects.models import Project
 
 
 class AWSSync:
@@ -23,7 +31,44 @@ class AWSSync:
         :return: True if function executes successfully
         """
         self.logger.info("Pressed button")
+        self.logger.info(self.get_emails_with_teamids())
         return True
+
+    def get_all_mailing_lists(self):
+        """
+        Get all mailing lists from the database.
+
+        :return: List of mailing lists
+        """
+        mailing_lists = MailingList.objects.all()
+        mailing_list_names = [ml.email_address for ml in mailing_lists]
+        return mailing_list_names
+
+    def get_emails_with_teamids(self):
+        """
+        Create a list of dictionaries containing email, slug and semester.
+
+        Slug and semester combined are together an uniqueness constraint.
+
+        :return: list of dictionaries of email, slug and semester
+        """
+        email_ids = []
+
+        for project in (
+            Project.objects.filter(mailinglist__isnull=False)
+            .filter(semester=Semester.objects.get_or_create_current_semester())
+            .values("slug", "semester", "mailinglist")
+        ):
+            project_slug = project["slug"]
+            project_semester = str(Semester.objects.get(pk=project["semester"]))
+            project_email = MailingList.objects.get(pk=project["mailinglist"]).email_address
+            email_dict = {
+                "project_email": project_email,
+                "project_slug": project_slug,
+                "project_semester": project_semester,
+            }
+            email_ids.append(email_dict)
+        return email_ids
 
     def create_aws_organization(self):
         """Create an AWS organization with the current user as the management account."""
